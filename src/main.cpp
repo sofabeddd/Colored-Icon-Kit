a2436741d24a806cca963368121488be6e718e74
sofa <146020160+sofabeddd@users.noreply.github.com>
2024-02-23 04:28:16 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <Geode/modify/GJGarageLayer.hpp>
 #include <Geode/modify/GJItemIcon.hpp>
 #include <Geode/modify/LeaderboardsLayer.hpp>
+#include <Geode/modify/LevelInfoLayer.hpp>
+#include <Geode/modify/MenuLayer.hpp>
 #include <Geode/modify/ProfilePage.hpp>
 #include <Geode/modify/SimplePlayer.hpp>
 
@@ -12,8 +14,8 @@ using namespace geode::prelude;
 bool override_glow = true;
 
 class $modify(CharacterColorPage) {
-    void updateIconColors() {
-        CharacterColorPage::updateIconColors();
+    void onClose(CCObject* sender) {
+        CharacterColorPage::onClose(sender);
 
         auto garage_layer = dynamic_cast<GJGarageLayer*>(CCDirector::sharedDirector()->getRunningScene()->getChildByID("GJGarageLayer"));
 
@@ -25,6 +27,15 @@ class $modify(CharacterColorPage) {
     }
 };
 
+class $modify(GJGarageLayer) {
+    void onBack(CCObject* sender) {
+        GJGarageLayer::onBack(sender);
+
+        override_glow = true;
+    }
+};
+
+
 class $modify(GJItemIcon) {
     static GJItemIcon* create(UnlockType p0, int p1, cocos2d::ccColor3B p2, cocos2d::ccColor3B p3, bool p4, bool p5, bool p6, cocos2d::ccColor3B p7) {
         auto game_manager = GameManager::get();
@@ -33,11 +44,36 @@ class $modify(GJItemIcon) {
     }
 };
 
+
 class $modify(LeaderboardsLayer) {
     bool init(LeaderboardState p0) {
         if (!LeaderboardsLayer::init(p0)) return false;
 
         override_glow = false;
+        return true;
+    }
+};
+
+class $modify(LevelInfoLayer) {
+    bool init(GJGameLevel* p0, bool p1) {
+        if (!LevelInfoLayer::init(p0, p1)) return false;
+
+        override_glow = true;
+        return true;
+    }
+
+    void onBack(CCObject* sender) {
+        LevelInfoLayer::onBack(sender);
+
+        override_glow = false;
+    }
+};
+
+class $modify(MenuLayer) {
+    bool init() {
+        if (!MenuLayer::init()) return false;
+
+        override_glow = true;
         return true;
     }
 };
@@ -54,7 +90,7 @@ class $modify(ProfilePage) {
         ProfilePage::onClose(sender);
 
         auto running_scene = CCDirector::sharedDirector()->getRunningScene();
-        if (!running_scene->getChildByID("LeaderboardsLayer") || !running_scene->getChildByID("LevelInfoLayer")) override_glow = true;
+        if (m_ownProfile) override_glow = true;
     }
 
     void onFriends(CCObject* sender) {
